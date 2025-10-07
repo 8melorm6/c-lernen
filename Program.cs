709fb374cff03ce858601ec3a28c5242beb0abfd
Michael Rommel <melorm@posteo.de>
2025-10-07 10:51:31 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_lernen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gebe deinen Vornamen ein: ");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Gebe deinen Nachnamen ein: ");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Gebe dein Alter ein: ");
+            byte age = Convert.ToByte(Console.ReadLine());
+        }
+    }
+}
