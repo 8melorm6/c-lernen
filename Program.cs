0b4dd9a786b5764495721ef5eb1dca1a926ab0cb
Michael Rommel <melorm@posteo.de>
2025-10-09 10:15:21 +0200

    else if

--- Program.cs
+++ Program.cs
@@ -2,37 +2,48 @@
 
 namespace c_lernen
 {
-
     class Program
     {
         static void Main(string[] args)
         {
-            int day = 2;
+            Console.Write("Gebe dein Gewicht in kg ein: ");
+            double weight = Convert.ToDouble(Console.ReadLine());
 
-            switch (day)
+            Console.Write("Gebe deine Größe in m ein: ");
+            double height = Convert.ToDouble(Console.ReadLine()); 
+
+            double bmi = weight / (height * height);
+            string classification = "";
+
+            if (bmi < 18.5)
             {
-                case 1:
-                    Console.WriteLine("Montag");
-                    break;
-                case 2:
-                    Console.WriteLine("Dienstag");
-                    break;
-                case 3:
-                    Console.WriteLine("Mittwoch");
-                    break;
-                case 4:
-                    Console.WriteLine("Donnerstag");
-                    break;
-                case 5:
-                    Console.WriteLine("Freitag");
-                    break;
-                case 6:
-                    Console.WriteLine("Samstag");
-                    break;
-                case 7:
-                    Console.WriteLine("Sonntag");
-                    break;
+                classification = "Untergewicht";
             }
+            else if (bmi >= 18.5 && bmi < 25)
+            {
+                classification = "Normalgewicht";
+            }
+            else if (bmi >= 25 && bmi < 30)
+            {
+                classification = "Übergewicht";
+            }
+            else if (bmi >= 30 && bmi < 35)
+            {
+                classification = "Adipositas Grad 1";
+            }
+            else if (bmi >= 35 && bmi < 40)
+            {
+                classification = "Adipositas Grad 2";
+            }
+            else if (bmi >= 40)
+            {
+                classification = "Adipositas Grad 3";
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dein BMI beträgt {0}, das ist {1}", bmi, classification);
+            Console.ReadKey();
+
         }
     }
 }
